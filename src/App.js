90ac994eaa2c9ad20cc34c14bef34cebbf602f76
rachidb5 <jordan.rachid@gmail.com>
2023-03-02 16:50:59 -0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
+import { HiMagnifyingGlass } from 'react-icons/hi2'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Previsão do tempo</h1>
+      <form action="/" method="GET" class="form">
+        <input type="search" placeholder="Insira aqui o nome da cidade" class="search-field" />
+        <button type="submit" class="search-button">
+          <HiMagnifyingGlass />
+        </button>
+      </form>
+      <h2>Capitais</h2>
     </div>
   );
 }
